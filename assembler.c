e43ebf2703a7aed3c236c8ced7a17a47e79ac191
Matthew Gibson <91528458+gibbyice@users.noreply.github.com>
2022-12-09 09:04:15 -0200

    Add files via upload

--- assembler.c
+++ assembler.c
@@ -1,46 +1,101 @@
 #include <stdio.h>  // required for access to file input-output functions in C
 #include <stdlib.h> // required for access to memory allocation functions
 #include <string.h> // required for validation on filename entry
+#include <unistd.h> // required to check if filename entered is blank
+#include <stdbool.h> // required for boolean stuff
+#include <string.h> // required for string stuff
 #include "assembler.h"
 
 // Gets the user to input the filename for the file to be assembled
-char[] getAssemblyFile(){
-    char filename[]; // initiallise variable to store filename
+bool getAssemblyFile(char filename[]){
     printf("Enter name of file to assemble: ");
     scanf("%s", filename);
-    bool exists = false;
     if (access(filename, F_OK) == 0) {
     	// checks if a file with user inputed name exists, as seen here https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
-		exists = true;
-		return filename;
+		return true;
 	} else {
-		printf("No file with name %s exists, aborting assembly proccess", filename);
-		return NULL;
+		printf("No file with name %s exists, aborting assembly proccess \n", filename);
+		return false;
 	}   
 }
 
-firstPass(Buffer *buffer, SymbolTable *SymbolTable, char[] filename){
-	
-}
+// Carries out the first pass in the analysis phase of the assembler
+/*bool firstPass(Buffer *buffer, SymbolTable *SymbolTable, const char *filename){
+	// Open file to read
+	FILE *fp = fopen("output.txt", "r");
+	// Check file opened correctly
+	if (fp!=NULL){
+		// define instruction set
+        int y = 0;
+        char line[128];
+        int currentAddress = 0;
+        // Loop through file indicated by filename
+        for(int i=0;fgets(line, 128, fp) != NULL;i++){
+        	if(line[0]=";"){
+        		// Skips iteration if the line is just a comment
+        		continue;
+        	}
+        	char * token = strtok(string, " "); // get first token
+        	while(token!=NULL){ // loop to get all tokens
+        		printf(" %s\n", token) // print token
+        		token = strtok(NULL," ");
+        	}
+        	//EXTENSION check syntax of line & instructions are valid
+        	//convert commands & numbers to machine code and send them to output buffer
+        	//if unrecognized variable, leave 2 lines blank in output buffer
+			//store any variables in symbol table
+			currentAddress++;
+		return true;
+        }
+    }	
+}*/
+
+// Carries out the second pass in the analysis phase of the assembler
+/*bool secondPass(Buffer *buffer, SymbolTable *SymbolTable, char[] filename){
+	//loop through file indicated by filename 
+		//check if previously unknown variable is now known
+		//if so fill in gap in input buffer
+		//else return false & print error message about reference to undeclaired variable
+		//return true
+}*/
+
+// Carries out the synthesis phase of the assembler and writes the output a file.
+/*void writeToFile(Buffer *buffer){
+	// Open file to write to (currently hard coded to output.txt)
+	FILE *fp = fopen("output.txt", "w");
+	// Loop through input buffer until the end or next line is blank
+	for(int i=0;i<BUFFER_SIZE;i++){
+		//line = (synthesise line);
+		if(fp!=NULL){
+	        writeRow(fp, line);
+	    }
+	}
+	fclose(fp);
+}*/
 
 // Carries out the assemly process
 void assemble(){
-	char[] filename = getAssemblyFile();
-	if(!strcmp(filename, NULL)){
-		// Checks filename isn't null, only occurs if no file matched entered filename
+    char filename[30] = {'\0'};
+	if (getAssemblyFile(filename)){
+		// Checks filename isn't empty, only occurs if no file matched entered filename
 		//initialise syboltable and output buffer
-		SymbolTable *table = (SymbolTable*)malloc(sizeof(SymbolTable));
-		Buffer *buffer = (Buffer*)malloc(sizeof(Buffer));
-		if(!(adjMatrixPtr==NULL || buffer==NULL)){
+		SymbolTable *table = NULL;
+		Buffer *buffer = NULL;
+		table = (SymbolTable*)malloc(sizeof(SymbolTable));
+		buffer = (Buffer*)malloc(sizeof(Buffer));
+		if(!(table==NULL || buffer==NULL)){
 			// Checks that no errors occurend during memory allocation
 			firstPass(*buffer, *SymbolTable, filename);
-			secondPass();
-        	
+			//secondPass(*buffer, *SymbolTable, filename);
+        	//writeToFile(*buffer);
     	} else {
-    		printf("Error occured durring memory allocation, aborting assembly proccess.")
-        	break
+    		printf("Error occured durring memory allocation, aborting assembly proccess.\n");
+        	return;
     	}
-	} else {
-		break;
 	}
+}
+
+int main()
+{
+	assemble();
 }
--- assembler.h
+++ assembler.h
@@ -1,27 +1,38 @@
 #ifndef __ASSEMBLER_HEADER__
 #define __ASSEMBLER_HEADER__
 
-// Data structure to store 2D array for symbol table
 // I don't know what size to actually make this :p
+#define SYMBOL_TABLE_SIZE 32
+// needs to be bigger as operands and commands are stored accross seperate lines
+#define BUFFER_SIZE 64
+
+// Data structure to store 2D array for symbol table
 typedef struct symbolTable{
-    char symbols[32][2];
+    char symbols[SYMBOL_TABLE_SIZE][2];
 } SymbolTable;
 
+
 typedef struct outputBuffer{
-	char buffer[32]
+	char buffer[BUFFER_SIZE];
 } Buffer;
 
 // Data structure to store 
-typdef struct instructionSet{
-	char instructions[8][2] = { "000",  "JMP"
-								"100",	"LRP"			
-								"010", 	"LDN"
-								"110",	"STO"
-								"001",	"SUB"
-								"101",	"SUB"
-								"011",	"CMP"
-								"111",	"STP"}
-}
+typedef struct instructionSet{
+	char instructions[8][2]; 
+} InstructionSet;
 
+/*char instructons[2][8] = { "000",  "JMP",
+					"100",	"LRP",
+					"010", 	"LDN",
+					"110",	"STO",
+					"001",	"SUB",
+					"101",	"SUB",
+					"011",	"CMP",
+					"111",	"STP"};*/
+
+void assemble();
+bool firstPass(Buffer *buffer, SymbolTable *SymbolTable, const char *filename);
+bool secondPass(Buffer *buffer, SymbolTable *SymbolTable, const char *filename);
+bool writeToFile(Buffer *buffer);
 
 #endif
